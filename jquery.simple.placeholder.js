40272650a4b33a7100c89d09f6ef327b362809fb
Joey Yore <jyore@gatech.edu>
2011-07-10 20:52:28 -0400

    Added Source

---
+++ jquery.simple.placeholder.js
@@ -0,0 +1,104 @@
+/**************************************************************************
+
+  jQuery Simple Placeholder Plugin
+
+  This plugin is designed to give a placeholder to text edit boxes.  
+  This placeholder will change colors when the user focuses/unfocuses 
+  the placeholder.  If the user inputs any character, then the placeholder
+  disappears and the key press registers as normal.  If the user leaves the
+  edit box blank, then the placeholder is restored.  The user inputted text
+  will not change colors.
+
+  Options:
+
+  colors : An object containing light and dark color options.
+  		light : the color that the placeholder changes to on focus
+		dark : the standard color of the text.
+
+  placeholder : A string that is used as the placeholder text.	
+
+
+  Example:
+
+  HTML
+  <div id='placeholder'></div>
+
+  JavaScript
+  $('#placeholder').placeholder();
+
+
+
+DISTRIBUTED AS OPEN SOURCE SOFTWARE UNDER THE MIT LICENSE - DETAILED BELOW:
+Copyright (c) 2011 Joey Yore
+
+Permission is hereby granted, free of charge, to any person obtaining a 
+copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the 
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included 
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+DEALINGS IN THE SOFTWARE.
+**************************************************************************/
+
+
+
+(function($){
+	$.fn.placeholder = function(options) {
+		
+		var settings = {
+			colors : {
+				light : '#aaa',
+				dark : '#000',
+			},
+			placeholder : 'Enter Something...',
+		}
+
+		return this.each(function() {
+			
+			var opts = $.extend({},settings,options);
+
+			var input = $('<input type="text" class="input-placeholder" value="' + opts.placeholder + '">').css('color',opts.colors.dark).appendTo($(this));
+
+			input.bind({
+				focus : function() {
+					$(this).css('color',opts.colors.light);
+				},
+				focusout : function() {
+					$(this).css('color',opts.colors.dark);
+
+					if($(this).val() === '') {
+						$(this).val(opts.placeholder);
+
+						$(this).bind({
+							keydown : function() {
+								$(this).val('');
+								$(this).css('color',opts.colors.dark);
+								$(this).unbind('keydown');
+								$(this).unbind('focus');
+							},
+							focus : function() {
+								$(this).css('color',opts.colors.light);
+							},
+						});
+					}
+				},
+				keydown : function() {
+					$(this).val('');
+					$(this).css('color',opts.colors.dark);
+					$(this).unbind('keydown');
+					$(this).unbind('focus');
+				}
+			});
+		});
+	};
+})(jQuery);
