b9dee538d60d18bdfcbe6fc939fb5954e9c399ad
jyore <joey@jyore.com>
2013-05-02 13:00:51 -0500

    Updated Code, Added LICENSE.txt, Updated REAME, Removed Unnecessary Files

--- jquery.simple.placeholder.js
+++ jquery.simple.placeholder.js
@@ -1,6 +1,6 @@
 /**************************************************************************
 
-  jQuery Simple Placeholder Plugin v0.1r2
+  jQuery Simple Placeholder Plugin v0.2r0
 
   This plugin is designed to give a placeholder to text edit boxes.  
   This placeholder will change colors when the user focuses/unfocuses 
@@ -12,10 +12,13 @@
   Options:
 
   colors : An object containing focus and normal color options.
+  		blur : the color that the placeholder
   		focus : the color that the placeholder changes to on focus
-		normal : the standard color of the text.
+		normal : the color of the input text.
 
   placeholder : A string that is used as the placeholder text.	
+
+  validate: A custom validation function.	
 
 
   Example:
@@ -50,64 +53,139 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
 DEALINGS IN THE SOFTWARE.
 **************************************************************************/
-
-
-
 (function($){
-	$.fn.placeholder = function(options) {
-		
-		var settings = {
-			colors : {
-				focus : '#aaa',
-				normal : '#000',
-			},
-			placeholder : 'Enter Something...',
-		}
+        
+    var settings = {
+        colors : {
+            blur : '#bbb',
+            focus : '#ddd',
+            normal : '#000'
+        },
+        placeholder : 'Enter Something...'
+    };
 
-		return this.each(function() {
-			
-			var opts = $.extend({},settings,options);
+    var methods = {
+        init : function(options) {
+            return this.each(function() {
+            
+                if(options !== undefined) {
+                    if('validate' in options) {
+                        methods.validate = options.validate;
+                    }
+                }
 
-			if($(this).is('div')) {
-				var input = $('<input type="text" class="input-placeholder" value="' + opts.placeholder + '">').css('color',opts.colors.normal).appendTo($(this));
-			} else {
-				var input = $(this);
-				$(this).addClass('input-placeholder').css('color',opts.colors.normal).val(opts.placeholder);
-			}
+                
+                var input;
+                var str = '';
+                var opts = {};
+                
+                if($(this).is('div')) {
+                    str = $(this).text();
+                    $(this).text('');
+                    
+                    
+                    if(str) {
+                        opts = $.extend({},settings,{placeholder:str},options);
+                    } else {
+                        opts = $.extend({},settings,options);
+                    } 
 
-			input.bind({
-				click : function() {
-					$(this).css('color',opts.colors.focus);
+                    input = $('<input type="text" class="input-placeholder" value="' + opts.placeholder + '">').css('color',opts.colors.blur).appendTo($(this));
+                } else {
+                    input = $(this);
+                    str = input.val();
+                    input.text('');
 
-					input[0].setSelectionRange(0,0);
-				},
-				focusout : function() {
-					$(this).css('color',opts.colors.normal);
+                    if(str) {
+                        opts = $.extend({},settings,{placeholder:str},options);
+                    } else {
+                        opts = $.extend({},settings,options);
+                    }
 
-					if($(this).val() === '') {
-						$(this).val(opts.placeholder);
+                    input.addClass('input-placeholder').css('color',opts.colors.blur).val(opts.placeholder);
+                }
 
-						$(this).bind({
-							keydown : function() {
-								$(this).val('');
-								$(this).css('color',opts.colors.normal);
-								$(this).unbind('keydown');
-								$(this).unbind('click');
-							},
-							click : function() {
-								$(this).css('color',opts.colors.focus);
-								input[0].setSelectionRange(0,0);
-							},
-						});
-					}
-				},
-				keydown : function() {
-					$(this).val('');
-					$(this).css('color',opts.colors.normal);
-					$(this).unbind('keydown');
-					$(this).unbind('click');
-				}
-			});
-		});
-	};
+                $.data($(this)[0],'changed',false);
+                input.bind({
+                    focusin : function() {
+                        $(this).css('color',opts.colors.focus);
+                    },
+                    mouseup : function() {
+                        if(!$.data($(this)[0],'changed')) {
+                            $(this)[0].setSelectionRange(0,0);
+                        }
+                
+                    },
+                    focusout : function() {
+                    
+                        if($(this).val() === '') {
+                            $(this).val(opts.placeholder);
+                            $.data($(this)[0],'changed',false);
+
+                            $(this).bind({
+                                keydown : function() {
+                                    $.data($(this)[0],'changed',true);
+                                    $(this).val('');
+                                    $(this).css('color',opts.colors.normal);
+                                    $(this).unbind('keydown');
+                                    $(this).unbind('focusin');
+                                },
+                                focusin : function() {
+                                    $(this).css('color',opts.colors.focus);
+                                    input[0].setSelectionRange(0,0);
+                                }
+                            });
+                        }
+
+                        if(!$.data($(this)[0],'changed')) {
+                            $(this).css('color',opts.colors.blur);
+                        }
+
+                    },
+                    keydown : function() {
+                        $.data($(this)[0],'changed',true);
+                        $(this).val('');
+                        $(this).css('color',opts.colors.normal);
+                        $(this).unbind('keydown');
+                        $(this).unbind('focusin');
+                    }
+                });
+            });
+        },
+        destroy : function() {
+            if($(this).is('div')) {
+                $(this).find('input').remove();
+            } else {
+                $(this).unbind('keydown').unbind('focusin').unbind('focusout').unbind('mouseup');
+                $(this).removeClass('input-placeholder');
+            }
+
+        },
+        length : function() {
+            if($(this).is('div')) {
+                return $(this).find('input').val().length;
+            } else {
+                return $(this).val().length;
+            }
+        },
+        isActive : function() {
+            return !$.data($(this)[0],'changed');
+        },
+        validate : function() {
+            return $.data($(this)[0],'changed');
+        }
+    };
+
+        
+    $.fn.placeholder = function(method) {
+        // Meithod calling logic
+        if ( methods[method] ) {
+            return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if ( typeof method === 'object' || ! method ) {
+            return methods.init.apply( this, arguments );
+        } else {
+            $.error( 'Method ' +  method + ' does not exist on jQuery.simple.placeholder' );
+        }
+    };
 })(jQuery);
+
