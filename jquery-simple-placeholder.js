44fdc0add92ad4c4ae18db8b59c202b63cf8af18
jyore <joey@jyore.com>
2013-05-30 08:31:04 -0500

    Fixed Doc website reference

--- jquery-simple-placeholder.js
+++ jquery-simple-placeholder.js
@@ -9,28 +9,32 @@
   edit box blank, then the placeholder is restored.  The user inputted text
   will not change colors.
 
-  Options:
 
-          colors : An object containing focus and normal color options.
-          blur : the color that the placeholder
-          focus : the color that the placeholder changes to on focus
-          normal : the color of the input text.
-
-  placeholder : A string that is used as the placeholder text.    
-
-  validate: A custom validation function.    
+  Documentation available at: http://jyore.com/opensource/jquery/simple-placeholder
 
 
-  Example:
+---DISTRIBUTED UNDER THE MIT LICENSE---
 
-  HTML
-  <div id='placeholder'></div>
-  <input id='placeholder2'>
+Copyright (c) 2011 Joey Yore
 
-  JavaScript
-  $('#placeholder').placeholder();
-  $('#placeholder2').placeholder();
+Permission is hereby granted, free of charge, to any person obtaining a 
+copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the 
+Software is furnished to do so, subject to the following conditions:
 
+The above copyright notice and this permission notice shall be included 
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+DEALINGS IN THE SOFTWARE.
+  
 **************************************************************************/
 (function($){
         
